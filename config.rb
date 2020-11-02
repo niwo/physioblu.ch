42dc3c3a63932b82325df91ba3c769f4e14e2555
Nik Wolfgramm <wolfgramm@puzzle.ch>
2020-11-02 12:26:11 +0100

    move _redirects to build folder on deploy

--- config.rb
+++ config.rb
@@ -19,3 +19,11 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+
+
+after_build do |builder|
+  src = File.join(File.dirname(__FILE__), "_redirects")
+  dst = File.join(config[:build_dir], "_redirects")
+  builder.thor.source_paths << File.dirname(__FILE__)
+  builder.thor.copy_file(src, dst)
+end
