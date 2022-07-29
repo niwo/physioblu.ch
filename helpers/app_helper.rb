b311922f52643f65e7a6e3cd646218da503a456f
Nik Wolfgramm <wolfgramm@puzzle.ch>
2022-07-29 19:27:52 +0200

    adding language switcher in top-menu

--- helpers/app_helper.rb
+++ helpers/app_helper.rb
@@ -1,6 +1,6 @@
 module AppHelper
 
-  #use frontmatter for I18n titles
+  # use frontmatter for I18n titles
   def self.page_title(page)
     if page.data.title_translation_key
       "#{I18n.t(page.data.title_translation_key)} - PhysioBlu"
@@ -14,6 +14,19 @@ module AppHelper
   def self.page_reference(page)
     ref = page.source_file.gsub(/.*localizable/, "")
     ref.gsub(".erb", "") 
+  end
+
+  def language_options
+    lang_opts = []
+    langs.each_with_index do |language, i|
+      if lang != language
+        lang_opts << {
+          lang: language,
+          title: @app.data.locales[language][:name]
+        }
+      end
+    end
+    lang_opts
   end
 
 end
